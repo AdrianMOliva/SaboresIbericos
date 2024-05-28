400129fa53a9eb2bbf58ca4d76339aafaf9ed174
Adrian Martin <martinolivaadrian@gmail.com>
2024-05-28 20:07:05 +0200

    readme

---
+++ Sabores-Ibericos/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <nav>
+        <a href="https://github.com/AdrianMOliva/SaboresIbericos/tree/main/Sabores-Ibericos"></a>
+      </nav>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Sabores-Ibericos/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import "./Navbar.css";
+function Navbar() {
+  return <div className="navbarclass"></div>;
+}
+
+export default Navbar;
