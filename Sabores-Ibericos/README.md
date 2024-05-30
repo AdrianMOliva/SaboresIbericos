57fb5da4139acfdc6361a64fda82243c4b67cef7
Adrian Martin <martinolivaadrian@gmail.com>
2024-05-30 21:23:27 +0200

    Some styling and data collected, created server

--- Sabores-Ibericos/README.md
+++ Sabores-Ibericos/README.md
@@ -1,1 +1,12 @@
 This is an App for Portuguese and Spanish food.
+
+Food/Plate
+
+Image
+City/Region (in the DetailsPage having a little map)
+Time of the meal(breakfast,snack,lunch...)
+
+Description
+Restaurants specialist?
+
+National dish tag?
--- Sabores-Ibericos/src/components/Footer.jsx
+++ Sabores-Ibericos/src/components/Footer.jsx
@@ -4,7 +4,9 @@ function Footer() {
   return (
     <div className="footer">
       <nav>
-        <a href="https://github.com/AdrianMOliva/SaboresIbericos/tree/main/Sabores-Ibericos"></a>
+        <a href="https://github.com/AdrianMOliva/SaboresIbericos/tree/main/Sabores-Ibericos">
+          Github Repository
+        </a>
       </nav>
     </div>
   );
